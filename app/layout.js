a578bf0c950111d5ea2194830a7578895934d435
pascall de creator <pascall.de.creator@gmail.com>
2023-06-30 14:26:48 +0100

    Update layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
-import Navbar from '@/components/navbar'
+import Navbar from '@/app/components/navbar'
+import Provider from '@/app/components/Provider'
 
 export const metadata = {
 	title: 'Workin | Find A Job For You',
@@ -7,11 +8,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className='w-screen bg-gray-100'>
-				<Navbar/>
-				<div className='container mx-auto mt-20 md:w'>
-					{children}
-				</div>
+			<body className='w-screen bg-gray-100 h-screen'>
+				<Provider>
+					<Navbar/>
+					<div className='h-full pt-24'>
+						{children}
+					</div>
+				</Provider>
 			</body>
 		</html>
 	)
