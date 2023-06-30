4ee7337f5c4737417118286f38f9af31624a0204
pascall de creator <pascall.de.creator@gmail.com>
2023-06-30 14:27:38 +0100

    Create Auth pages

---
+++ app/auth/signin/page.js
@@ -0,0 +1,19 @@
+import GoogleSignInButton from "@/app/components/googleSignInButton";
+
+const SignIn = () => {
+	return (
+		<div className="container mx-auto w-full mt-20 flex md:items-center justify-center">
+			<div className="flex flex-col w-full p-4 bg-white gap-2 rounded-lg">
+				<GoogleSignInButton/>
+				<hr/>
+				<label htmlFor="email">Email</label>
+				<input type="email" name="email" id="email" />
+				<label htmlFor="password">Password</label>
+				<input type="password" name="password" id="password" />
+				
+			</div>
+		</div>
+	);
+}
+
+export default SignIn;
