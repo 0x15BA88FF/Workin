a999a8efec81fe731dd0130f5b06c09dccc3c156
pascall de creator <pascall.de.creator@gmail.com>
2023-06-30 14:26:27 +0100

    Update page

--- app/page.js
+++ app/page.js
@@ -1,43 +1,57 @@
 "use client"
 import { useState, useEffect } from 'react'
-import ListingCard from '../components/jobCard.js';
-import FilterNav from '../components/filterNav.js';
-import SearchBox from '../components/searchBox.js';
+import ListingCard from './components/jobCard.js';
+import FilterNav from './components/filterNav.js';
+import SearchBox from './components/searchBox.js';
 
 const Page = () => {
-    const [jobData, setJobData] = useState(null)
-    const [isJobDataLoading, setJobDataLoading] = useState(true)
+    const [jobsData, setJobsData] = useState(null)
+    const [isJobsDataLoading, setJobsDataLoading] = useState(true)
+    
+    const [profileData, setProfileData] = useState(null)
+    const [isProfileDataLoading, setProfileDataLoading] = useState(true)
+
+    useEffect(() => {
+        fetch(`/api/profiles/649d4fcecbe80beee7904b58`)
+            .then((res) => res.json())
+            .then((data) => {
+                setProfileData(data)
+                setProfileDataLoading(false)
+            })
+    }, [])
 
     useEffect(() => {
         fetch('/api/jobs')
             .then((res) => res.json())
             .then((data) => {
-                setJobData(data)
-                setJobDataLoading(false)
+                setJobsData(data)
+                setJobsDataLoading(false)
             })
     }, [])
 
     return (
-        <div className="flex my-4 gap-4">
+        <div className="container mx-auto flex gap-4">
             <FilterNav/>
-            <div className="md:right-Listing">
+            <div className="listing">
                 <SearchBox/>
                     {
-                        isJobDataLoading?
-                                <>Loading...</>
-                        : jobData?
-                        <>
-                                <p className="my-5 text-gray-600">{ jobData.length } Result(s) found</p>
-                                <div className="grid grid-flow-row grid-cols-1 lg:grid-cols-2 gap-4">
-                                {
-                                    jobData.map((job) => (
-                                        <ListingCard key={ job.id } data={ job }/> 
+                        isJobsDataLoading?
+                                <div className='flex justify-center'>
+                                    <p className='mt-20'>Loading...</p>
+                                </div>
+                        : jobsData?
+                            <>
+                                    <p className="my-5 text-gray-600">{ jobsData.length } Result(s) found</p>
+                                    <div className="grid grid-flow-row grid-cols-1 lg:grid-cols-2 gap-4">
+                                    {
+                                        jobsData.map((jobData) => (
+                                            <ListingCard key={ jobData.id } jobData={ jobData } profileData={ profileData }/> 
                                         ))
-                                    }
-                                </div>  
-                            </>
+                                        }
+                                    </div>  
+                                </>
                         :
-                        <>No data found</>
+                            <p className="my-5 text-gray-600">0 results found</p>
                     }
             </div>
         </div>
